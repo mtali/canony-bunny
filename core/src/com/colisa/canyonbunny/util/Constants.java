6bf9320235fb881fc26603f6e023ab7ec5445ee5
mtali <mtali.emmanuel@protonmail.com>
2017-07-09 14:50:35 +0300

    Add textual packer location constant

--- core/src/com/colisa/canyonbunny/util/Constants.java
+++ core/src/com/colisa/canyonbunny/util/Constants.java
@@ -5,4 +5,7 @@ public class Constants {
     // Visible game word is 5 meters wide and 5 meters tall
     public static final float VIEWPORT_WIDTH = 5;
     public static final float VIEWPORT_HEIGHT = 5;
+
+    // Location of description file for texture atlas
+    public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.pack.atlas";
 }
