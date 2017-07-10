e341d944d40cb241fac04909ed368fab00ff1648
mtali <mtali.emmanuel@protonmail.com>
2017-07-10 13:31:40 +0300

    Implement rock render method

--- core/src/com/colisa/canyonbunny/game/objects/Rock.java
+++ core/src/com/colisa/canyonbunny/game/objects/Rock.java
@@ -35,6 +35,79 @@ public class Rock extends AbstractGameObject {
 
     @Override
     public void render(SpriteBatch batch) {
+        TextureRegion region = null;
+
+        float relX = 0;
+        float relY = 0;
+
+        // Draw left edge
+        region = regionEdge;
+        relX -= dimension.x / 4;
+        batch.draw(
+                region.getTexture(),
+                position.x + relX,
+                position.y + relY,
+                origin.x,
+                origin.y,
+                dimension.x / 4,
+                dimension.y,
+                scale.x,
+                scale.y,
+                rotation,
+                region.getRegionX(),
+                region.getRegionY(),
+                region.getRegionWidth(),
+                region.getRegionHeight(),
+                false,
+                false
+        );
+
+        // Draw middle
+        relX = 0;
+        region = regionMiddle;
+        for (int i = 0; i < length; i++) {
+            batch.draw(
+                    region.getTexture(),
+                    position.x + relX,
+                    position.y + relY,
+                    origin.x,
+                    origin.y,
+                    dimension.x,
+                    dimension.y,
+                    scale.x,
+                    scale.y,
+                    rotation,
+                    region.getRegionX(),
+                    region.getRegionY(),
+                    region.getRegionWidth(),
+                    region.getRegionHeight(),
+                    false,
+                    false
+
+            );
+            relX += dimension.x;
+        }
+
+        // Draw right edge
+        region = regionEdge;
+        batch.draw(
+                region.getTexture(),
+                position.x + relX,
+                position.y + relY,
+                relX + dimension.x/8,
+                origin.y,
+                dimension.x / 4,
+                dimension.y,
+                scale.x,
+                scale.y,
+                rotation,
+                region.getRegionX(),
+                region.getRegionY(),
+                region.getRegionWidth(),
+                region.getRegionHeight(),
+                true,
+                false
+        );
 
     }
 }
