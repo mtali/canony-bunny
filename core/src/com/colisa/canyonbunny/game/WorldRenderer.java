4bb83ce39cf7d242085732ab3fdacd809413d9e6
mtali <mtali.emmanuel@protonmail.com>
2017-07-13 01:34:02 +0300

    Integrate camera-gui to world render for  displaying score, extra lives , fps on right positions

--- core/src/com/colisa/canyonbunny/game/WorldRenderer.java
+++ core/src/com/colisa/canyonbunny/game/WorldRenderer.java
@@ -1,13 +1,19 @@
 package com.colisa.canyonbunny.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
 import com.colisa.canyonbunny.util.Constants;
 
 
 public class WorldRenderer implements Disposable {
+
+    private final static String TAG = WorldRenderer.class.getName();
     private OrthographicCamera camera;
+    // Camera to for viewing score, lives and game performance(FPS)
+    private OrthographicCamera cameraGUI;
     private SpriteBatch batch;
     private WorldController worldController;
 
@@ -21,10 +27,16 @@ public class WorldRenderer implements Disposable {
         camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
         camera.position.set(0, 0, 0);
         camera.update();
+
+        // initializing GUI camera
+        cameraGUI = new OrthographicCamera(Constants.VIEWPORT_GUI_WIDTH, Constants.VIEWPORT_GUI_HEIGHT);
+        cameraGUI.setToOrtho(true);
+        cameraGUI.update();
     }
 
     public void render() {
         renderWorld(batch);
+        renderGui(batch);
     }
 
     private void renderWorld(SpriteBatch batch) {
@@ -35,9 +47,91 @@ public class WorldRenderer implements Disposable {
         batch.end();
     }
 
+    private void renderGuiScore(SpriteBatch batch) {
+        float x = -15;
+        float y = -15;
+        batch.draw(Assets.instance.goldCoinAssets.goldCoin,
+                x,
+                y,
+                50,
+                50,
+                100,
+                100,
+                0.35f,
+                -0.35f,
+                0
+        );
+        Assets.instance.assetFonts.defaultBig.draw(
+                batch,
+                String.valueOf(worldController.score),
+                x + 75 ,
+                y + 37
+        );
+    }
+
+    private void renderGuiExtraLive(SpriteBatch batch) {
+        float x = cameraGUI.viewportWidth - 50 - Constants.LIVES_START * 50;
+        float y = -15;
+        for (int i = 0; i < Constants.LIVES_START; i++) {
+            if (worldController.lives <= i)
+                batch.setColor(0.5f, 0.5f, 0.5f, 0.5f);
+            batch.draw(
+                    Assets.instance.bunnyAssets.head,
+                    x + i * 50,
+                    y,
+                    50,
+                    50,
+                    129,
+                    100,
+                    0.35f,
+                    -0.35f,
+                    0
+            );
+            batch.setColor(1, 1, 1, 1);
+        }
+    }
+
+    private void renderGuiFpsCounter(SpriteBatch batch) {
+        float x = cameraGUI.viewportWidth - 55;
+        float y = cameraGUI.viewportHeight - 15;
+        int fps = Gdx.graphics.getFramesPerSecond();
+        BitmapFont fpsFont = Assets.instance.assetFonts.defaultNormal;
+        if (fps >= 45) {
+            // 45 or more show green
+            fpsFont.setColor(0, 1, 0, 1);
+        } else if (fps >= 30) {
+            // 30 or more FPS show up in yellow
+            fpsFont.setColor(1, 1, 0, 1);
+        }else {
+            // less than 30 show up in red
+            fpsFont.setColor(1, 0, 0, 1);
+        }
+
+        fpsFont.draw(batch, "FPS: " + fps, x, y);
+        fpsFont.setColor(1, 1, 1, 1); // reset font color
+    }
+
+    private void renderGui(SpriteBatch batch){
+        batch.setProjectionMatrix(cameraGUI.combined);
+        batch.begin();
+        renderGuiScore(batch);
+        renderGuiExtraLive(batch);
+        renderGuiFpsCounter(batch);
+        batch.end();
+    }
+
     public void resize(int width, int height) {
-        camera.viewportWidth = (Constants.VIEWPORT_HEIGHT / height) * width;
+        float ratio = (float) width / (float) height;
+
+        // updating world camera aspects ratio
+        camera.viewportWidth = Constants.VIEWPORT_HEIGHT * ratio;
         camera.update();
+
+        // updating camera GUI aspect ratio
+        cameraGUI.viewportHeight = Constants.VIEWPORT_GUI_HEIGHT;
+        cameraGUI.viewportWidth = Constants.VIEWPORT_GUI_HEIGHT * ratio;
+        cameraGUI.position.set(cameraGUI.viewportWidth / 2, cameraGUI.viewportHeight / 2, 0);
+        cameraGUI.update();
     }
 
     @Override
