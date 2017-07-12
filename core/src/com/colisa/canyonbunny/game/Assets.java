25738048e470094a3ac180a75aac3bc60714c172
mtali <mtali.emmanuel@protonmail.com>
2017-07-12 14:02:07 +0300

    Dispose font assets

--- core/src/com/colisa/canyonbunny/game/Assets.java
+++ core/src/com/colisa/canyonbunny/game/Assets.java
@@ -68,6 +68,7 @@ public class Assets implements Disposable, AssetErrorListener {
 
     @Override
     public void dispose() {
+        assetFonts.dispose();
         assetManager.dispose();
     }
 
