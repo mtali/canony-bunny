af286b68ea374b23993773ff3b5a84073de6f86e
mtali <mtali.emmanuel@protonmail.com>
2017-07-14 01:57:14 +0300

    Add code to handle calculations and switching of states needed to enable jumping and falling

--- core/src/com/colisa/canyonbunny/game/objects/BunnyHead.java
+++ core/src/com/colisa/canyonbunny/game/objects/BunnyHead.java
@@ -1,22 +1,21 @@
 package com.colisa.canyonbunny.game.objects;
 
-
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.colisa.canyonbunny.game.Assets;
+import com.colisa.canyonbunny.util.Constants;
 import com.colisa.canyonbunny.util.Enums.JUMP_STATE;
 import com.colisa.canyonbunny.util.Enums.VIEW_DIRECTION;
 
 @SuppressWarnings("WeakerAccess")
 public class BunnyHead extends AbstractGameObject {
     private static final String TAG = BunnyHead.class.getName();
-    private TextureRegion region;
-
     public VIEW_DIRECTION viewDirection;
     public JUMP_STATE jumpState;
     public float timeJumping;
     public boolean hasFeatherPowerUp;
     public float timeLeftFeatherPowerUp;
+    private TextureRegion region;
 
     public BunnyHead() {
         init();
@@ -42,8 +41,95 @@ public class BunnyHead extends AbstractGameObject {
         bounds.set(0, 0, dimension.x, dimension.y);
     }
 
+    public void setJumping(boolean jumpKeyPressed) {
+        switch (jumpState) {
+            case GROUNDED:
+                if (jumpKeyPressed) {
+                    timeJumping = 0;
+                    jumpState = JUMP_STATE.JUMP_RISING;
+                }
+                break;
+            case JUMP_RISING:
+                if (!jumpKeyPressed)
+                    jumpState = JUMP_STATE.FALLING;
+                break;
+            case FALLING:
+            case JUMP_FALLING:
+                if (jumpKeyPressed && hasFeatherPowerUp) {
+                    timeJumping = Constants.JUMP_TIME_OFFSET_FLYING;
+                    jumpState = JUMP_STATE.JUMP_RISING;
+                }
+                break;
+        }
+    }
+
+    public void setFeatherPowerUp(boolean picked) {
+        hasFeatherPowerUp = true;
+        if (picked) {
+            timeLeftFeatherPowerUp = Constants.ITEM_FEATHER_POWERUP_DURATION;
+        }
+    }
+
+    public boolean hasFeatherPowerUp() {
+        return hasFeatherPowerUp && timeLeftFeatherPowerUp > 0;
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        if (velocity.x != 0) {      // meaning the bunny head is moving
+            viewDirection = velocity.x > 0 ? VIEW_DIRECTION.RIGHT : VIEW_DIRECTION.LEFT;
+        }
+
+        // Update timeLeftFeatherPowerUp
+        if (hasFeatherPowerUp()) {
+            timeLeftFeatherPowerUp -= deltaTime;
+            if (timeLeftFeatherPowerUp < 0) {
+                timeLeftFeatherPowerUp = 0;
+                setFeatherPowerUp(false);
+            }
+        }
+    }
+
+    /**
+     * WHY are we overriding this method
+     * 1. Update the jumping state
+     * 2. Update velocity
+     * 3. Update jump time on each frame
+     */
+    @Override
+    protected void updateMotionY(float deltaTime) {
+        switch (jumpState) {
+            case GROUNDED:
+                jumpState = JUMP_STATE.FALLING;
+                break;
+
+            case JUMP_RISING:
+                timeJumping += deltaTime;
+                if (timeJumping < Constants.JUMP_TIME_MAX) {
+                    // Still jumping
+                    velocity.y = terminalVelocity.y;
+                }
+                break;
+
+            case FALLING:
+                break;
+
+            case JUMP_FALLING:
+                timeJumping += deltaTime;
+                if (timeJumping > 0 && timeJumping <= Constants.JUMP_TIME_MIN) {
+                    // Still jumping
+                    velocity.y = terminalVelocity.y;
+                }
+        }
+        if (jumpState != JUMP_STATE.GROUNDED)
+            super.updateMotionY(deltaTime);
+    }
+
     @Override
     public void render(SpriteBatch batch) {
+        if (hasFeatherPowerUp)
+            batch.setColor(1.0f, 0.8f, 0.0f, 1.0f);
 
         batch.draw(
                 region.getTexture(),
@@ -60,7 +146,7 @@ public class BunnyHead extends AbstractGameObject {
                 region.getRegionY(),
                 region.getRegionWidth(),
                 region.getRegionHeight(),
-                false,
+                viewDirection == VIEW_DIRECTION.LEFT,
                 false
         );
         // reset color to white
